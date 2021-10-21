365f0267f330d23175006957aac95b060dcab5e2
Vaibhav <vaibhav8726@gmail.com>
2021-10-21 15:24:44 +0530

    Updates

---
+++ precache-manifest.7908719b24756ae6605d4510fc9de128.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe8070fe619ae9124b1fb321be2d2f6",
+    "url": "/spotify-web-app/index.html"
+  },
+  {
+    "revision": "876bc9a6ab6bcac08935",
+    "url": "/spotify-web-app/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "eebcf084453d0d869b00",
+    "url": "/spotify-web-app/static/css/main.41df2c0c.chunk.css"
+  },
+  {
+    "revision": "876bc9a6ab6bcac08935",
+    "url": "/spotify-web-app/static/js/2.184633ba.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/spotify-web-app/static/js/2.184633ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebcf084453d0d869b00",
+    "url": "/spotify-web-app/static/js/main.57491969.chunk.js"
+  },
+  {
+    "revision": "aada2453d57ed4a51487",
+    "url": "/spotify-web-app/static/js/runtime-main.06073429.js"
+  },
+  {
+    "revision": "1b2651924062685b5a98dbdb6331a52b",
+    "url": "/spotify-web-app/static/media/music.1b265192.jpeg"
+  }
+]);
