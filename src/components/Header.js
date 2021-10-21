d8b28e2d172d9b8594a887e5c674ba1ace6c5792
Vaibhav <vaibhav8726@gmail.com>
2021-10-21 14:59:14 +0530

    deploy spotify web app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-  return <h1 className="main-heading text-center">Spotify Music Search</h1>;
+  return <h1 className="main-heading text-center">Spotify Web App</h1>;
 };
 
 export default Header;
